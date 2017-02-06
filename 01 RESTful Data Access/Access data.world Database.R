737969be7e47a3cb3bc63745f1d0df1c412539fd
pcannata <cannata@cs.utexas.edu>
2017-02-05 19:18:55 -0600

    Cause the notebook,RMD and Access data.world Database.R to work with a global variable names redacted

--- 01 RESTful Data Access/Access data.world Database.R
+++ 01 RESTful Data Access/Access data.world Database.R
@@ -2,7 +2,7 @@ require("jsonlite")
 require("RCurl")
 
 # Change the USER and PASS below to be your credentials. Get your password from Edit profile -> Settings -> Advanced -> Copy API Token to clipboard. Also change cannata:test-emp-and-dept to yourAccount:yourDataset.
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5000/rest/native/?query="select * from emp"')),httpheader=c(DB='jdbc:data:world:sql:cannata:test-emp-and-dept', USER='cannata', PASS='redacted', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5000/rest/native/?query="select * from emp"')),httpheader=c(DB='jdbc:data:world:sql:cannata:test-emp-and-dept', USER='cannata', PASS=redacted, MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 print(summary(df))
 print(head(df))
 
