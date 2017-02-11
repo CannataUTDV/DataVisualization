3bd853ab3d195b416a60f50b9447e760135b6beb
pcannata <cannata@cs.utexas.edu>
2017-02-11 16:39:31 -0600

    Cleaned up ggplot function

--- 02 Grammar of Graphics with R and ggplot2/Plotting Function Example.R
+++ 02 Grammar of Graphics with R and ggplot2/Plotting Function Example.R
@@ -24,18 +24,17 @@ ggplot_func <- function(df,
     names(df)[names(df) == Legend] <- 'Legend'
     print(names(df))
     
-    p1 <- ggplot(df) + 
+    p <- ggplot(df) + 
       geom_point(aes(x=carat, y=price, colour=Legend)) +
       ylim(YMin, YMax) + xlim(XMin, XMax) +
       scale_colour_manual(values = PointColor) +
       labs(title="Grammar of Graphics Example", x = paste("carat\nFigure ", FigureNum), colour = Legend, caption = Caption) +
       theme(axis.text.x=element_text(size=20, vjust=0.5)) +
       theme(axis.text.y=element_text(size=20, hjust=0.5)) +
-      theme(axis.title=element_text(size=14, face="bold")) +
       theme(plot.title = element_text(size=22)) +
       theme(legend.title = element_text(size=20, face="bold"), legend.text = element_text(size=18))
  
-    return(p1)
+    return(p)
 }
 
 p1 <- ggplot_func(diamonds)
@@ -43,7 +42,7 @@ p2 <- ggplot_func(diamonds, YMin = 5000, YMax = 15000, Caption = "This is a subs
 p3 <- dplyr::filter(diamonds, cut == "Premium") %>% ggplot_func(., Legend = "cut")
 p4 <- ggplot_func(diamonds, Legend = "clarity", PointColor = c("black", "purple", "red", "blue", "green", "yellow", "grey", "orange"))
 
-library("grid")
+require("grid")
 
 # You may need to use getwd() to find out where the "4diamonds.png" file will be put.
 # You can use setwd() to set the current working directory.
