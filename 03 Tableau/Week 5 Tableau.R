2fb7f9546f9929a24298c27407cabd9c329f1e54
pcannata <cannata@cs.utexas.edu>
2017-02-14 22:15:59 -0600

    Wrote a comment to show how to run my REST server on localhost.

--- 03 Tableau/Week 5 Tableau.R
+++ 03 Tableau/Week 5 Tableau.R
@@ -1,4 +1,16 @@
-df <- read.csv("/Users/pcannata/Downloads/SuperStoreOrders.csv", stringsAsFactors = FALSE)
+# df <- read.csv("/Users/pcannata/Downloads/SuperStoreOrders.csv", stringsAsFactors = FALSE)
+
+'
+I loaded my REST server onto my Mac so I could run it exclusively during class.
+To do this:
+   Download my REST server and unzip it
+   cd to the unzipped folder
+   cd CarnotDist/CarnotRE
+   ../dist/bin/jython -J-Xmx1g restful_start.py
+   then use localhost:5011 below
+'
+   
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'localhost:5011/rest/native/?query="select * from SuperStoreOrders"')),httpheader=c(DB='jdbc:data:world:sql:cannata:superstoreorders', USER='cannata', PASS=redacted, MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
 summary(df)
 head(df)
 
