dc8b106b2ac4e4aa7c6d78e08b075b7672a3dfba
pcannata <cannata@cs.utexas.edu>
2017-02-23 11:13:20 -0600

    Updated 03 Tableau fold with file for box plots and histograms.

--- 03 Tableau/Week 5 - R of Tableau Boxplots and Histograms.R
+++ 03 Tableau/Week 5 - R of Tableau Boxplots and Histograms.R
@@ -17,3 +17,7 @@ head(df)
 p = dplyr::filter(df, Region == "International" | Region == "East") %>% ggplot(.) + geom_boxplot(aes(x=Category, y=Sales, colour=Region)) +
   theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5))
 print(p)
+
+p = dplyr::filter(df, Region == "International" | Region == "East") %>% ggplot(.) + geom_histogram(aes(x=Shipping_Cost)) +
+  theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5))
+print(p)
