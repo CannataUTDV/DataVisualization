086445d08278680cf718d1d550a62c7685807328
pcannata <cannata@cs.utexas.edu>
2017-04-10 11:35:53 -0500

    Added a space after the button.

--- 09TableauExamples/ui.R
+++ 09TableauExamples/ui.R
@@ -21,7 +21,8 @@ dashboardPage(
                       min = 0, max = .1,  value = .1),
           sliderInput("KPI2", "KPI_Medium:", 
                       min = .1, max = .2,  value = .2),
-          actionButton(inputId = "click1",  label = "To start, click here"),
+          actionButton(inputId = "click1",  label = "To get data, click here"),
+          hr(), # Add space after button.
           DT::dataTableOutput("data1")
         ),
         tabPanel("Crosstab", plotOutput("plot1", height=1000))
